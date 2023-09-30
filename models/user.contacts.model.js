ccca7ef7c08069a94842d038fd2347ff3ae81243
clintjeff2 <clintjeff2@gmail.com>
2023-09-30 08:21:15 +0100

    Modified contacts model and created the populate userContactInfo on Logged in user

--- models/user.contacts.model.js
+++ models/user.contacts.model.js
@@ -5,6 +5,10 @@ const userContactSchema = new mongoose.Schema({
 		type: mongoose.Types.ObjectId,
 		ref: 'User',
 		required: [true, 'A user must own a contact list'],
+		unique: [
+			true,
+			'Already exist, each user only has a single contact card for all their contacts',
+		],
 	},
 	contacts: [
 		{
@@ -12,14 +16,28 @@ const userContactSchema = new mongoose.Schema({
 				type: mongoose.Types.ObjectId,
 				ref: 'User',
 				required: [true, 'Users should be added to a contact list'],
+				// unique: [
+				// 	true,
+				// 	'Cannot add a contact that already exist in the contact card',
+				// ],
 			},
 			dateAdded: {
 				type: Date,
 				default: new Date(Date.now()),
 			},
+			contactName: String,
 		},
 	],
 });
 
+//Query middleware to populate users on userContactSchema, also set contactName to user.username if contactName is void
+userContactSchema.pre('find', function (next) {
+	this.populate({ path: 'contacts.user', select: '_id username photo' });
+	// if(!this.contacts.contactName) this.contacts.contactName =
+
+	next();
+});
+
+
 const UserContacts = mongoose.model('User_Contact', userContactSchema);
-module.exports = UserCotnacts;
+module.exports = UserContacts;
