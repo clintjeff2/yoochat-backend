f0c23836b88f66a0fe1e942dcdb46dd587cc4f9f
clintjeff2 <clintjeff2@gmail.com>
2023-10-02 09:17:43 +0100

    group.contacts.controller.js don't know what to do yet

---
+++ controllers/group.contacts.controller.js
@@ -0,0 +1,10 @@
+const GroupContacts = require('./../models/group.contacts.model');
+const ErrorApi = require('./../utilities/ErrorApi');
+const catchAsync = require('./../utilities/catchAsync');
+const sendResponse = require('./../utilities/sendResponse');
+
+exports.createGroup = catchAsync(async (req, res, next) => {});
+exports.deleteGroup = catchAsync(async (req, res, next) => {});
+exports.updateGroup = catchAsync(async (req, res, next) => {});
+exports.getGroups = catchAsync(async (req, res, next) => {});
+exports.getAllGroups = catchAsync(async (req, res, next) => {});
